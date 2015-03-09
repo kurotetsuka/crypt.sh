ac4bd35518d8f9cebef152669e27e58563d36877
kurotetsuka <kurotetsuka@gmail.com>
2015-03-08 21:39:52 -0400

    got vault working and added some informative output

--- crypt.sh
+++ crypt.sh
@@ -1,33 +1,45 @@
 #!/bin/bash
 
 CRYPTSH_safe_dev="/dev/mmcblk0p2"
-CRYPTSH_vault_dev="/dev/sda"
+CRYPTSH_vault_dev="/dev/sdb"
 CRYPTSH_vault_key="/mnt/safe/keys/crypt/zero"
 CRYPTSH_open_status="\e[1;36m open \e[0m"
 CRYPTSH_closed_status="\e[1;31mclosed\e[0m"
 
 unlock-safe(){
-	sudo cryptsetup open /dev/mmcblk0p2 safe &&
-	sudo mount /mnt/safe
+	echo -e "Unlocking Safe..."
+	sudo cryptsetup open $CRYPTSH_safe_dev safe &&
+	sudo mount /mnt/safe &&
+	echo -e "Safe unlocked" ||
+	echo -e "Failed to unlock Safe"
 }
 
 unlock-vault(){
-	[[ -e /dev/mapper/safe ]] || unlock-safe &&
+	[[ -e /dev/mapper/safe ]] || unlock-safe
+	echo -e "Unlocking Vault..."
 	sudo cryptsetup open \
-		--key-file CRYPTSH_vault_key \
-		CRYPTSH_vault_dev vault &&
-	sudo mount /mnt/vault
+		--key-file $CRYPTSH_vault_key \
+		$CRYPTSH_vault_dev vault &&
+	sudo mount /mnt/vault &&
+	echo -e "Vault unlocked" ||
+	echo -e "Failed to unlock Vault"
 }
 
 lock-safe(){
-	[[ -e /dev/mapper/vault ]] && lock-vault &&
+	echo -e "Locking Safe..."
+	[[ -e /dev/mapper/vault ]] && lock-vault
 	sudo umount /mnt/safe &&
-	sudo cryptsetup close safe
+	sudo cryptsetup close safe &&
+	echo -e "Safe locked" ||
+	echo -e "Failed to lock Safe"
 }
 
 lock-vault(){
+	echo -e "Locking Vault..."
 	sudo umount /mnt/vault &&
-	sudo cryptsetup close vault
+	sudo cryptsetup close vault &&
+	echo -e "Vault locked" ||
+	echo -e "Failed to lock Vault"
 }
 
 status-crypt(){
@@ -37,36 +49,39 @@ status-crypt(){
 	[[ -e /dev/mapper/vault ]] &&
 		vault_status=$CRYPTSH_open_status ||
 		vault_status=$CRYPTSH_closed_status
-	echo -en "\e[1m  safe :: $safe_status\n"
-	echo -en "\e[1m vault :: $vault_status\n"
+	echo -e "\e[1m  safe :: $safe_status"
+	echo -e "\e[1m vault :: $vault_status"
 }
 
 status-safe(){
 	[[ -e /dev/mapper/safe ]] &&
 		safe_status=$CRYPTSH_open_status ||
 		safe_status=$CRYPTSH_closed_status
-	echo -en "\e[1m  safe :: $safe_status\n"
+	echo -e "\e[1m  safe :: $safe_status"
 }
 
 status-vault(){
 	[[ -e /dev/mapper/vault ]] &&
 		vault_status=$CRYPTSH_open_status ||
 		vault_status=$CRYPTSH_closed_status
-	echo -en "\e[1m vault :: $vault_status\n"
+	echo -e "\e[1m vault :: $vault_status"
 }
 
 setup-safe(){
+	echo -e "Creating Safe..."
 	sudo cryptsetup -v \
 		-c serpent-xts-plain64 \
 		-s 512 \
 		-h whirlpool \
 		-i 5000 \
 		--use-random \
-		luksFormat $CRYPTSH_safe_dev \
-		#--debug
+		luksFormat $CRYPTSH_safe_dev  &&
+	echo -e "Vault created" ||
+	echo -e "Failed to create Vault"
 }
 
 setup-vault(){
+	echo -e "Creating Vault..."
 	sudo cryptsetup -v \
 		-c serpent-xts-plain64 \
 		-s 512 \
@@ -74,6 +89,7 @@ setup-vault(){
 		-i 5000 \
 		--use-random \
 		luksFormat $CRYPTSH_vault_dev \
-		$CRYPTSH_vault_key \
-		#--debug
+		$CRYPTSH_vault_key  &&
+	echo -e "Vault created" ||
+	echo -e "Failed to create Vault"
 }
